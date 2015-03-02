e879aefdc97979a1762964e7fadfb9ea6279e18d
xenyztw <chaussende.adrien@gmail.com>
2015-03-02 14:20:50 +0100

    ACE: Setting up Server module and class. Add Maven integration

---
+++ Server/src/main/java/com/polytech4A/pop3/server/core/Server.java
@@ -0,0 +1,77 @@
+package com.polytech4A.pop3.server.core;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by Adrien on 02/03/2015.
+ *
+ * @author Adrien
+ * @version 1.0
+ *          <p/>
+ *          Application server management.
+ */
+public class Server {
+
+    /**
+     * Server's socket.
+     */
+    private ServerSocket socket;
+    /**
+     * Logger of the server.
+     */
+    private static Logger logger = Logger.getLogger(Server.class);
+
+    /**
+     * Getter of server's socket.
+     *
+     * @return socket ServerSocket
+     */
+    public ServerSocket getSocket() {
+        return socket;
+    }
+
+    /**
+     * Starts the server's socket on the port with nbConnections allowed.
+     *
+     * @param port          Server port number.
+     * @param nbConnections Number of connections allowed on the server.
+     */
+    public void start(int port, int nbConnections) {
+        try {
+            this.socket = new ServerSocket(port, nbConnections);
+            logger.info("Starting serveur on port " + port + ".");
+        } catch (IOException e) {
+            logger.error("Impossible to start server, port may " + port + " be busy");
+            logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Socket waits for a connection and then starts a thread with the pro
+     */
+    public void openConnection() {
+        try {
+            this.socket.accept();
+        } catch (IOException e) {
+            logger.error("Impossible to open a connection");
+            logger.error(e.getMessage());
+        }
+
+    }
+
+    /**
+     * Close the socket.
+     */
+    public void close() {
+        try {
+            this.socket.close();
+        } catch (IOException e) {
+            logger.error("Impossible to close server");
+            logger.error(e.getMessage());
+        }
+    }
+
+}
---
+++ Server/src/main/java/com/polytech4A/pop3/server/core/state/State.java
@@ -0,0 +1,8 @@
+package com.polytech4A.pop3.server.core.state;
+
+/**
+ * Created by Adrien on 02/03/2015.
+ */
+public abstract class State {
+
+}
