e702992a8f04108b82c1f3751df967e46719288b
xenyztw <chaussende.adrien@gmail.com>
2015-03-04 11:26:05 +0100

    ACE : Add begin of Connection test class

---
+++ Server/src/test/java/com/polytech4A/pop3/server/ConnectionTest.java
@@ -0,0 +1,14 @@
+package com.polytech4A.pop3.server;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Adrien on 04/03/2015.
+ *
+ * @author Adrien
+ * @version 1.0
+ *
+ * Test case for Connection class.
+ */
+public class ConnectionTest extends TestCase {
+}
