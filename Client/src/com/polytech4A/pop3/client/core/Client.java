d3270b038ffb467cb34fd05b9bf3a6deb105d3b7
PReynaud <pierre.reynaud@hotmail.fr>
2015-03-04 23:38:37 +0100

    [PRD] Client can go in first state, waiting for a message and do the action defined in this state

--- Client/src/com/polytech4A/pop3/client/core/Client.java
+++ Client/src/com/polytech4A/pop3/client/core/Client.java
@@ -1,5 +1,10 @@
 package com.polytech4A.pop3.client.core;
 
+import com.polytech4A.pop3.client.core.state.State;
+import com.polytech4A.pop3.client.core.state.StateStarted;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Observable;
@@ -9,6 +14,7 @@ import java.util.Observable;
  */
 public class Client extends Observable implements Runnable {
     private ClientConnection connection;
+    private State currentState;
 
     public Client() {
     }
@@ -22,10 +28,10 @@ public class Client extends Observable implements Runnable {
     }
 
     /**
-     * Make the connection with the server
+     * Make the connection with the server and call the constructor of the first state (Started)
      */
     public void establishConnection(){
-        //TO DO Change adress and port to get them with the view
+        // TODO : Change adress and port to get them with the view
         InetAddress address = null;
         int port = 500;
         try {
@@ -35,6 +41,7 @@ public class Client extends Observable implements Runnable {
         }
 
         this.connection = new ClientConnection(address, port);
+        this.currentState = new StateStarted();
     }
 
     /**
@@ -50,7 +57,43 @@ public class Client extends Observable implements Runnable {
         this.processing();
     }
 
+    /**
+     * Allow the transition between states if the required conditions are OK
+     */
     private void processing(){
+        String response = this.waitForResponse();
 
+        if(this.currentState.analyze(response)){
+            this.currentState.action();
+            this.currentState = this.currentState.getNextState();
+        }
+        else{
+            //retry or send error
+        }
     }
+
+    /**
+     * Will wait for the response and send back the response with a string format
+     */
+    private String waitForResponse(){
+        StringBuilder response = new StringBuilder();
+        BufferedInputStream bi = this.connection.getBufferedInputStream();
+
+        try {
+            /* We are going to wait until the response arrived */
+            /* TODO : Timeout here */
+            while (bi.available()==0){
+
+            }
+            while (bi.available() != 0) {
+                response.append((char) bi.read());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response.toString();
+    }
+
+
 }
--- Client/src/com/polytech4A/pop3/client/core/state/StateStarted.java
+++ Client/src/com/polytech4A/pop3/client/core/state/StateStarted.java
@@ -4,24 +4,25 @@ package com.polytech4A.pop3.client.core.state;
  * Created by Pierre on 04/03/2015.
  */
 
+
 /*
  * State connected of the client: waiting for the Welcome message of the server and will goo to next state (Authentication)
  */
 public class StateStarted extends State{
-    //if message = "OK $nomServer POP3 server ready"
-    //go to next state
-
     public StateStarted(){
     }
 
 
     @Override
     public boolean analyze(String message) {
-        return false;
+        Boolean response = false;
+        //response = new OkMessage().matches(message);
+        return response;
     }
 
     @Override
     public void action() {
 
+        this.setNextState(new StateAuthentification());
     }
 }
