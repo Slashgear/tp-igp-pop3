6841ec1764f04e6a292610d4fd7ac9b4002b309f
xenyztw <chaussende.adrien@gmail.com>
2015-03-04 11:45:11 +0100

    ACE: Server class implementation. Provide methods to call for server management.

--- Server/src/main/java/com/polytech4A/pop3/server/core/Server.java
+++ Server/src/main/java/com/polytech4A/pop3/server/core/Server.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by Adrien on 02/03/2015.
@@ -36,12 +37,22 @@ public class Server {
     }
 
     /**
+     * Server constructor. Start the server by creating a socket.
+     *
+     * @param port          Server will listen on this port.
+     * @param nbConnections Maximum number of connections allowed on the server.
+     */
+    public Server(int port, int nbConnections) {
+        this.start(port, nbConnections);
+    }
+
+    /**
      * Starts the server's socket on the port with nbConnections allowed.
      *
-     * @param port          Server port number.
-     * @param nbConnections Number of connections allowed on the server.
+     * @param port          Server will listen on this port.
+     * @param nbConnections Maximum number of connections allowed on the server.
      */
-    public void start(int port, int nbConnections) {
+    private void start(int port, int nbConnections) {
         try {
             this.socket = new ServerSocket(port, nbConnections);
             logger.info("Starting serveur on port " + port + ".");
@@ -54,18 +65,13 @@ public class Server {
     /**
      * Socket waits for a connection and then starts a thread with the pro
      */
-    public void openConnection() {
-        try {
-            this.socket.accept();
-        } catch (IOException e) {
-            logger.error("Impossible to open a connection");
-            logger.error(e.getMessage());
-        }
-
+    public void openConnection(Socket socket) {
+        Thread connectionThread = new Thread(new Connection(socket));
+        connectionThread.start();
     }
 
     /**
-     * Close the socket.
+     * Closes the socket.
      */
     public void close() {
         try {
@@ -73,6 +79,21 @@ public class Server {
         } catch (IOException e) {
             logger.error("Impossible to close server");
             logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Starts the server process and open a connection when socket accepts a connection.
+     */
+    public void listen() {
+        //TODO : Handle a better exit condition.
+        while (true) {
+            try {
+                this.openConnection(this.socket.accept());
+            } catch (IOException e) {
+                logger.error("Error during connection accepting");
+                logger.error(e.getMessage());
+            }
         }
     }
 
