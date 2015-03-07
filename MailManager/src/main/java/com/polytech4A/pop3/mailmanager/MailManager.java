b3d789bf6cacd1cc9c433a069f8816639391924a
Dimitri <d.rodarie@gmail.com>
2015-03-07 16:34:40 +0100

    [DRE] Add User and Mail Management :     MailManager.java     User.java     MailManagerException

--- MailManager/src/main/java/com/polytech4A/pop3/mailmanager/MailManager.java
+++ MailManager/src/main/java/com/polytech4A/pop3/mailmanager/MailManager.java
@@ -1,10 +1,79 @@
 package com.polytech4A.pop3.mailmanager;
 
+import java.util.ArrayList;
+
 /**
  * Created by Dimitri on 03/03/2015.
  */
 public class MailManager {
 
-    private User[] users;
+    private ArrayList<User> users;
 
+    public MailManager() {
+        this.users =User.getUsers();
+    }
+
+    public MailManager(User user){
+        this.users = new ArrayList<User>();
+        this.users.add(user);
+    }
+
+    public boolean initUser(User user){
+        int index = users.indexOf(user);
+        if (index>=0&&!users.get(index).isLocked()){
+            user.lockUser();
+            user.initMails();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isLockedUser(User user){
+        int index = users.indexOf(user);
+        if (index>=0&&!users.get(index).isLocked()){
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Mail> getMails (User user){
+        int index = users.indexOf(user);
+        if (index>=0&&users.get(index).isLocked()){
+
+            ArrayList<Mail> mails = users.get(index).getMails();
+            for (Mail mail : users.get(index).getMails()){
+                users.get(index).deleteMail(mail);
+            }
+        }
+        return null;
+    }
+
+    public void unlockUser(User user){
+        int index = users.indexOf(user);
+        if (index>=0&&users.get(index).isLocked()){
+            users.get(index).unlockUser();
+        }
+    }
+
+    public String createMail (User user, String receiver, String content, String subject){
+        int index = users.indexOf(user);
+        if (index>=0&&users.get(index).isLocked()){
+            return users.get(index).createMail( receiver, content,  subject);
+        }
+        return null;
+    }
+
+    public void addMail (Mail mail,User user){
+        int index = users.indexOf(user);
+        if (index>=0&&users.get(index).isLocked()){
+            users.get(index).addMail(mail);
+        }
+    }
+
+    public void saveMails (User user){
+        int index = users.indexOf(user);
+        if (index>=0&&users.get(index).isLocked()){
+            users.get(index).saveMails();
+        }
+    }
 }
--- MailManager/src/main/java/com/polytech4A/pop3/mailmanager/User.java
+++ MailManager/src/main/java/com/polytech4A/pop3/mailmanager/User.java
@@ -1,8 +1,12 @@
 package com.polytech4A.pop3.mailmanager;
 
+import com.polytech4A.pop3.mailmanager.Exceptions.MailManagerException;
+import com.polytech4A.pop3.mailmanager.Exceptions.MalFormedMailException;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Created by Dimitri on 04/03/2015.
@@ -18,6 +22,16 @@ public class User {
         this.mails=new ArrayList<Mail>();
     }
 
+    public User(String login) {
+        this.login = login;
+        this.password="";
+        this.mails=new ArrayList<Mail>();
+    }
+
+    public ArrayList<Mail> getMails() {
+        return mails;
+    }
+
     public void addMail(Mail mail){
         mails.add(mail);
     }
@@ -26,55 +40,92 @@ public class User {
         mails.remove(mail);
     }
 
-    public boolean findUser(){
-        try{
-            InputStream ips=new FileInputStream("Mails/logins.txt");
-            InputStreamReader ipsr=new InputStreamReader(ips);
-            BufferedReader br=new BufferedReader(ipsr);
-            String line;
-            String[] ident;
+    public boolean isLocked (){
+        File f = new File("Mails/"+login+"/lock.txt");
+        return f.exists() && !f.isDirectory();
+    }
 
-            while ((line=br.readLine())!=null){
-                ident=line.split(" ");
-                if(login==ident[0]&&password==ident[1]){
-                    return true;
-                }
+    public boolean lockUser (){
+        File f = new File("Mails/"+login+"/lock.txt");
+        if(!f.exists()&& !f.isDirectory()) {
+            try {
+                return f.createNewFile();
+            } catch (IOException e) {
+                MailManagerException ex = new MailManagerException("User.lockUser : Can't create file : Mails/"+login+"/lock.txt");
+                System.out.println(ex.getMessage());
             }
-            br.close();
-        }
-        catch (Exception e){
-            System.out.println(e.toString());
         }
         return false;
     }
 
-    public void saveUser(){
-        StringBuffer line = new StringBuffer(login);
-        line.append(" ");
-        line.append(password);
-        try {
-            BufferedWriter fichier = new BufferedWriter(new FileWriter("Mails/logins.txt", true));
-            fichier.newLine();
-            fichier.write(line.toString());
-            fichier.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public boolean unlockUser(){
+        File f = new File("Mails/"+login+"/lock.txt");
+        if(f.exists()&& !f.isDirectory()) {
+            if (f.delete()){
+                return true;
+            }
         }
+        return false;
     }
 
     public void saveMails (){
         Date date = new Date();
 
         try {
-            BufferedWriter fichier = new BufferedWriter(new FileWriter("Mails/"+login+"/mails_"+date.toString()+".txt", true));
-            for (int i=0; i<mails.size(); i++){
-                fichier.write(mails.get(i).getOutput().toString());
-                fichier.newLine();
+            BufferedWriter file = new BufferedWriter(new FileWriter("Mails/"+login+"/mails_"+date.toString()+".txt", true));
+            for (Mail mail : mails) {
+                file.write(mail.getOutput().toString());
+                file.newLine();
             }
-            fichier.close();
+            file.close();
 
         } catch (IOException e) {
-            e.printStackTrace();
+            MailManagerException ex = new MailManagerException("User.saveMail : Can't create file : Mails/"+login+"/mails_"+date.toString()+".txt");
+            System.out.println(ex.getMessage());
         }
+    }
+
+    public static ArrayList<User> getUsers(){
+        ArrayList<User> users = new ArrayList<User>();
+        try{
+            InputStream ips=new FileInputStream("Mails/logins.txt");
+            InputStreamReader ipsr=new InputStreamReader(ips);
+            BufferedReader br=new BufferedReader(ipsr);
+            String line;
+            String[] identification;
+
+            while ((line=br.readLine())!=null){
+                identification=line.split(" ");
+                users.add(new User(identification[0],identification[1]));
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            MailManagerException ex = new MailManagerException("User.getUser : Can't open file : Mails/logins.txt");
+            System.out.println(ex.getMessage());
+        }
+        return users;
+    }
+
+    public void initMails(){
+        File folder = new File ("Mails/"+login);
+        try{
+            for (File fileEntry : folder.listFiles()) {
+                if (!fileEntry.isDirectory()) {
+                    mails.add(new Mail(new Scanner(new File(fileEntry.getName())).useDelimiter("\\Z").next()));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            MailManagerException ex = new MailManagerException("User.initMails : File in 'Mails/"+login+"' not found");
+            System.out.println(ex.getMessage());
+        } catch (MalFormedMailException e) {
+            MailManagerException ex = new MailManagerException("User.InitMail : " + e.getMessage());
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public String createMail(String receiver, String content,String subject){
+        Mail mail = new Mail(this.login, receiver,content,subject);
+        return mail.getOutput().toString();
     }
 }
